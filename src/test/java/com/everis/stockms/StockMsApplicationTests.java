7dc5d21730c9d713dd49eaa51758d067bfe89f97
Ronald Soto <rsotoy_15@hotmail.com>
2020-09-18 18:23:16 -0500

    Subiendo archivos en repositorio

---
+++ src/test/java/com/everis/stockms/StockMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.stockms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
